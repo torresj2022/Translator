771784c90761d7519537ee12f30b5602eda95dfb
torresj2022 <124199047+torresj2022@users.noreply.github.com>
2023-02-02 12:44:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,3 @@
-from deep_translator import GoogleTranslator
-import pandas as pd
-file = pd.read_excel("LATAM.xlsx")
-file = file.fillna('None')
-def translate(x):
-    try:
-        y = GoogleTranslator(source='auto', target='en').translate(x)
-    except:
-        y = "did not translate"
-    return y
-file['translated'] = file['Q2'].apply(lambda x: translate(x)) 
-file['translatedQ3'] = file['Q3'].apply(lambda x: translate(x))
-file.to_excel("translation.xlsx")
+import streamlit as st
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
