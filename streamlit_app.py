c7058d65fecac2764e4ba6a7309fa79635270885
torresj2022 <124199047+torresj2022@users.noreply.github.com>
2023-02-02 15:56:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
+from deep_translator import GoogleTranslator
 import streamlit as st
 import pandas as pd
 
-st.write(st.text_input("text to translate", key="text"))
+def translate(x):
+    try:
+        y = GoogleTranslator(source='auto', target='en').translate(x)
+    except:
+        y = "did not translate"
+    return y
+ 
+st.write(translate(st.text_input("text to translate", key="text")))
