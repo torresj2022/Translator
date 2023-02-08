9c5ce6141fce79cd0f9117534593e1b7914a31da
torresj2022 <124199047+torresj2022@users.noreply.github.com>
2023-02-08 11:36:21 -0500

    Update and rename streamlit_app.py to translator.py

--- translator.py
+++ translator.py
@@ -16,7 +16,7 @@ def translate(x):
    
  def convert_df(df):
      # IMPORTANT: Cache the conversion to prevent computation on every rerun
-     return df.to_csv().encode('utf-8')
+    return df.to_csv().encode('utf-8')
         
 uploaded_file = st.file_uploader("Please add the csv/excel file to translate")
 if uploaded_file is not None:
