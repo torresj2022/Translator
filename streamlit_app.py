03d7be06c45f5345d8fdf11611aa14000598576e
torresj2022 <124199047+torresj2022@users.noreply.github.com>
2023-02-08 09:12:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ def translate(x):
         y = "did not translate"
     return y
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Please add the excel file to translate")
 if uploaded_file is not None:
 ###x =  st.text_input("Please add file path", value ="", key="text")
     # Can be used wherever a "file-like" object is accepted:
@@ -25,8 +25,8 @@ if uploaded_file is not None:
     columns)
     #file = pd.read_excel(x)
     file = file.fillna('None')
-    file = file.iloc[:10]
+    file = file.iloc[:100]
     if y: 
        for i in range(0,len(y)): 
             file["translation"+y[i]] = file.apply(lambda x: translate(x[y[i]]), axis = 1)
-            st.table(file["translation"+y[i]])
+            st.table(file[[y[i], "translation"+y[i]]])
