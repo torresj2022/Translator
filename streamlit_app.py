57ca2de828ca3d75027bb440a8f81ab8c902dd07
torresj2022 <124199047+torresj2022@users.noreply.github.com>
2023-02-08 08:55:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 from deep_translator import GoogleTranslator
 import streamlit as st
 import pandas as pd
+from io import StringIO
 
 def translate(x):
     try:
@@ -8,18 +9,24 @@ def translate(x):
     except:
         y = "did not translate"
     return y
-x =  st.text_input("Please add file path", value ="", key="text")
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None
+###x =  st.text_input("Please add file path", value ="", key="text")
+    # Can be used wherever a "file-like" object is accepted:
+    file = pd.read_csv(uploaded_file)
+    
 
-if x != '':
-    file = pd.read_excel(x)
+###if x != '':
+    ##file = pd.read_excel(x)
     columns = file.columns
     y =  st.multiselect(
     'What are the columns to translate',
     columns)
     file = pd.read_excel(x)
     file = file.fillna('None')
-    if y: 
-        for i in range(0,len(y)): 
-            file["translation"+y[i]] = file.apply(lambda x: translate(x[y[i]]), axis = 1)
-            st.table(file["translation"+y[i]])
+    st.write(file)
+    ##if y: 
+       ## for i in range(0,len(y)): 
+        ##    file["translation"+y[i]] = file.apply(lambda x: translate(x[y[i]]), axis = 1)
+         ##   st.table(file["translation"+y[i]])
